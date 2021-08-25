b9810338f8ecd10cc38938c316cd2c614bc41cbe
Arohi37 <62218383+Arohi37@users.noreply.github.com>
2021-08-25 19:55:02 +0530

    Add files via upload

---
+++ my_module.py
@@ -0,0 +1,12 @@
+def multiply(a,b):
+    mul=(a*b)
+    print(mul)
+
+def division(c,d):
+    div=(c/d)
+    print(div)
+    print(__name__)
+
+if __name__ == '__main__':
+    multiply(2,3)
+    division(4,4)
